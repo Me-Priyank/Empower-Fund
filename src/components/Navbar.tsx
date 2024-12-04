955d059c79c5e856509bb3a3f904da9c0f64a360
Me Priyank <pvonlyone29@gmail.com>
2024-12-04 23:33:54 +0530

    navbar optimized

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,6 @@ export default function Navbar() {
           {/* Desktop menu */}
           <div className="hidden sm:flex sm:items-center sm:space-x-8">
             <Link to="/causes" className="text-gray-700 hover:text-primary">Causes</Link>
-            <Link to="/donate" className="text-gray-700 hover:text-primary">Donate</Link>
             <Link to="/volunteer" className="text-gray-700 hover:text-primary">Volunteer</Link>
             <Link to="/donate" className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary-dark transition">
               Donate Now
